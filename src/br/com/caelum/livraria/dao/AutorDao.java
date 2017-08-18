bc53c122fec2154c4fabe74e816439d5d210238b
Higor Nucci <higornucci@gmail.com>
2017-08-18 15:56:04 -0400

    Trabalhando com um banco de dados configurado no AS

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
 
-	@Inject
-	private Banco banco;
+	@PersistenceContext
+	private EntityManager entityManager;
 	
 	@PostConstruct
 	void aposCriacao() {
@@ -20,16 +21,15 @@ public class AutorDao {
 	}
 
 	public void salva(Autor autor) {
-		banco.save(autor);
+		entityManager.persist(autor);
 	}
 	
 	public List<Autor> todosAutores() {
-		return banco.listaAutores();
+		return entityManager.createQuery("select a from Autor a", Autor.class).getResultList();
 	}
 
 	public Autor buscaPelaId(Integer autorId) {
-		Autor autor = this.banco.buscaPelaId(autorId);
-		return autor;
+		return this.entityManager.find(Autor.class, autorId);
 	}
 	
 }
